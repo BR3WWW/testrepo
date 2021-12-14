61e8a347fce8c32e66c69670828bbba7fbd09332
BR3WWW <95650513+BR3WWW@users.noreply.github.com>
2021-12-14 14:39:08 -0500

    Add files via upload

---
+++ default_workspace/contracts/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+    bool favoriteBool;
+
+    struct People {
+        uint favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function addPerson(string memory _name, uint _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+    }
+    
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+
+    }
+
+    function retrieve() public view returns(uint256) {
+        return(favoriteNumber);
+    }
+
+}
