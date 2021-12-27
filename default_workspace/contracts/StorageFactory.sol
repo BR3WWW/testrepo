82a0bc249091c3f449be7450cb1e48a700ab3887
"BR3WWW" <"llocalla.coroton@gmail.com">
2021-12-27 18:08:02 -0500

    Gottaleaveamsg

--- default_workspace/contracts/StorageFactory.sol
+++ default_workspace/contracts/StorageFactory.sol
@@ -14,13 +14,11 @@ contract StorageFactory is SimpleStorage {
     }
 
     function sfStore (uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
-        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        simpleStorage.store(_simpleStorageNumber);
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
     }
 
     function sfGet (uint256 _simpleStorageIndex) public view returns(uint256) {
-        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        return simpleStorage.retrieve();
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
     }
 
 }
