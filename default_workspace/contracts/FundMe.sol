6f0b34a33deaf89e0ca93a56b9c56c5bfedb4278
"BR3WWW" <"llocalla.coroton@gmail.com">
2021-12-28 17:25:27 -0500

    EndLesson3

--- default_workspace/contracts/FundMe.sol
+++ default_workspace/contracts/FundMe.sol
@@ -9,11 +9,18 @@ contract FundMe {
     using SafeMath for uint256;
 
     mapping (address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         uint256 minimumUSD = 50 * 10 ** 18;
         require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     function getVersion() public view returns(uint256){
@@ -33,8 +40,18 @@ contract FundMe {
         return ethAmountInUsd;
     }
 
-    function withdraw() payable public {
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() payable onlyOwner public {
         payable(msg.sender).transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
     }
 
 }
